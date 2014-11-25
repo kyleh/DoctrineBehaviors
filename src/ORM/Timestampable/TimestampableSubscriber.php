b372775587fb4bc9176b5855b37c8e1525832c13
Kyle Hendricks <kyleh@mendtechnologies.com>
2014-11-24 17:55:05 -0700

    change created to modified in subscriber

--- src/ORM/Timestampable/TimestampableSubscriber.php
+++ src/ORM/Timestampable/TimestampableSubscriber.php
@@ -49,7 +49,7 @@ class TimestampableSubscriber extends AbstractSubscriber
                 $classMetadata->addLifecycleCallback('updateTimestamps', Events::preUpdate);
             }
 
-            foreach (array('created', 'updated') as $field) {
+            foreach (array('created', 'modified') as $field) {
                 if (!$classMetadata->hasField($field)) {
                     $classMetadata->mapField(array(
                         'fieldName' => $field,
